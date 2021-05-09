100932930493f1a2b50a6304c55c9c1c8caaa1cf
Menno Buggenhout <mennobuggenhout@gmail.com>
2021-05-09 23:15:27 +0200

    bladi

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,39 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-  plugins: [],
+ module.exports = {
+  siteMetadata: {
+    title: `Four Aces`,
+    description: `Site of the Four Aces Guitar Quartet`,
+    author: `@achilles`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `four-aces`,
+        short_name: `FAGQ`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
